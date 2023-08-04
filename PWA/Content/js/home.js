633269259187158fd2e075116a3e293840ec785b
anhduymm1 <duybui100073@gmail.com>
2023-08-04 16:28:31 +0700

    blabla

--- PWA/Content/js/home.js
+++ PWA/Content/js/home.js
@@ -1,84 +1,84 @@
 ﻿$("document").ready(function () {
-    getListDesk();
+    getListDeskByWhere(-1);
     statiticDesk();
 });
 
-function getListDesk() {
-    $.ajax({
-        url: '/getListDesk',
-        type: 'POST',
-        dataType: 'json',
-        success: function (data) {
-            var containerDiv = $('.row-cols-lg-3'); // Thay thế bằng selector của container thích hợp
+//function getListDesk() {
+//    $.ajax({
+//        url: '/getListDesk',
+//        type: 'POST',
+//        dataType: 'json',
+//        success: function (data) {
+//            var containerDiv = $('.row-cols-lg-3'); // Thay thế bằng selector của container thích hợp
 
-            data.forEach(function (item, index) {
-                var cardDiv = $('<div>', {
-                    class: 'col my-3'
-                });
+//            data.forEach(function (item, index) {
+//                var cardDiv = $('<div>', {
+//                    class: 'col my-3'
+//                });
 
-                var card = $('<div>', {
-                    class: 'card border-hover-primary hover-scale'
-                });
+//                var card = $('<div>', {
+//                    class: 'card border-hover-primary hover-scale'
+//                });
 
-                var cardBody = $('<div>', {
-                    class: 'card-body'
-                });
+//                var cardBody = $('<div>', {
+//                    class: 'card-body'
+//                });
 
-                var textPrimary = $('<div>', {
-                    class: 'text-primary mb-5'
-                });
+//                var textPrimary = $('<div>', {
+//                    class: 'text-primary mb-5'
+//                });
 
-                var imageDiv = $('<div>', {
-                    style: 'border: 2px solid black; border-radius:50px;'
-                });
+//                var imageDiv = $('<div>', {
+//                    style: 'border: 2px solid black; border-radius:50px;'
+//                });
 
-                var image = $('<img>', {
-                    src: './images/lunch-time.png',
-                    style: 'width: 60px; height: 60px;'
-                });
+//                var image = $('<img>', {
+//                    src: './images/lunch-time.png',
+//                    style: 'width: 60px; height: 60px;'
+//                });
 
-                var h4 = $('<h4>', {
-                    class: 'font-weight-bold mb-3',
-                    text: item.TableName
-                });
-                var trangthai = ''
+//                var h4 = $('<h4>', {
+//                    class: 'font-weight-bold mb-3',
+//                    text: item.TableName
+//                });
+//                var trangthai = ''
 
-                if (item.TableStatus == '1') {
-                    trangthai = 'Bàn đã đặt'
-                }
-                else {
-                    trangthai = 'Bàn chưa đặt'
-                }
+//                if (item.TableStatus == '1') {
+//                    trangthai = 'Bàn đã đặt'
+//                }
+//                else {
+//                    trangthai = 'Bàn chưa đặt'
+//                }
 
-                var p = $('<p>', {
-                    class: 'text-muted mb-0',
-                    text: trangthai
-                });
+//                var p = $('<p>', {
+//                    class: 'text-muted mb-0',
+//                    text: trangthai
+//                });
 
-                if (trangthai === "Bàn đã đặt") {
-                    card.css('background-color', '#C8E4B2');
-                }
+//                if (trangthai === "Bàn đã đặt") {
+//                    card.css('background-color', '#C8E4B2');
+//                }
 
-                card.on('click', function () {
-                    var userFormModal = new bootstrap.Modal(document.getElementById("userformmodal"));
-                    userFormModal.show();
-                });
+//                card.on('click', function () {
+//                    var userFormModal = new bootstrap.Modal(document.getElementById("userformmodal"));
+//                    userFormModal.show();
+//                });
 
-                imageDiv.append(image);
-                textPrimary.append(imageDiv);
-                cardBody.append(textPrimary, h4, p);
-                card.append(cardBody);
-                cardDiv.append(card);
+//                imageDiv.append(image);
+//                textPrimary.append(imageDiv);
+//                cardBody.append(textPrimary, h4, p);
+//                card.append(cardBody);
+//                cardDiv.append(card);
 
-                containerDiv.append(cardDiv);
-            });
-        },
-        error: function () {
-            // Xử lý lỗi nếu có
-            console.log('Không thể lấy dữ liệu từ API.');
-        }
-    });
-}
+//                containerDiv.append(cardDiv);
+//            });
+//        },
+//        error: function () {
+//            // Xử lý lỗi nếu có
+//            console.log('Không thể lấy dữ liệu từ API.');
+//        }
+//    });
+//}
 
 function statiticDesk() {
     var all = document.getElementById("static-all")
@@ -191,8 +191,6 @@ $('input[name="table-status"]').on('change', function () {
     var containerDiv = $('.row-cols-lg-3');
     containerDiv.empty(); // Xóa hết nội dung trong containerDiv
     getListDeskByWhere(selectedValue)
-    statiticDesk()
-   
 });
 
 document.getElementById("userformmodal").addEventListener("show.bs.modal", function () {
--- PWA/Controllers/ProductApiController.cs
+++ PWA/Controllers/ProductApiController.cs
@@ -13,12 +13,17 @@ namespace PWA.Controllers
     {
         [Route("getListProduct")]
         [HttpPost]
-        public IHttpActionResult getListProduct()
+        public IHttpActionResult getListProduct(Products product1)
         {
             List<Products> products = new List<Products>();
             using (SqlConnection connection = new SqlConnection(PhoController.connectionString))
             {
-                string sqlQuery = "select P.*, C.CategoryName from Product P INNER JOIN Category C ON P.CateID = C.CategoryID";
+                string sqlQuery = "SELECT P.*, C.CategoryName from Product P INNER JOIN Category C ON P.CateID = C.CategoryID where P.CateID = '" + product1.CateID + "' order by P.ProductID desc ";
+
+                if (product1.CateID == -1)
+                {
+                    sqlQuery = "select P.*, C.CategoryName from Product P INNER JOIN Category C ON P.CateID = C.CategoryID order by P.ProductID desc";
+                }
                 using (SqlCommand command = new SqlCommand(sqlQuery, connection))
                 {
                     connection.Open();
@@ -43,5 +48,133 @@ namespace PWA.Controllers
             }
             return Json(products);
         }
+
+        public string getMaxIDProduct()
+        {
+            string maxID = "";
+            using (SqlConnection connection = new SqlConnection(PhoController.connectionString))
+            {
+                string sqlQuery = "SELECT max(ProductID) FROM Product";
+                using (SqlCommand command = new SqlCommand(sqlQuery, connection))
+                {
+                    connection.Open();
+                    maxID = command.ExecuteScalar().ToString();
+
+                }
+            }
+            if (maxID == "") maxID = "0";
+            return maxID;
+        }
+
+        public bool checkProductIDUnique(int productID)
+        {
+            bool flat = false;
+            using (SqlConnection connection = new SqlConnection(PhoController.connectionString))
+            {
+                string sqlQuery = "SELECT top (1) * FROM Product where ProductID = '" + productID + "'";
+                using (SqlCommand command = new SqlCommand(sqlQuery, connection))
+                {
+                    connection.Open();
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            flat = true;
+                        }
+                    }
+
+                }
+            }
+            return flat;
+        }
+
+        [Route("createProduct")]
+        [HttpPost]
+        public bool createProduct(Products products)
+        {
+            bool check = false;
+
+            using (SqlConnection connection = new SqlConnection(PhoController.connectionString))
+            {
+                string sqlQuery = @"INSERT INTO Product (ProductID, CateID , ProductName,ProductPrice, ProductUnit) VALUES
+                                      ('" + (Convert.ToInt32(getMaxIDProduct()) + 1).ToString() + "', '" + products.CateID + "','" + products.ProductName + "','" + products.ProductPrice + "','" + products.ProductUnit + "')";
+                try
+                {
+                    using (SqlCommand command = new SqlCommand(sqlQuery, connection))
+                    {
+                        connection.Open();
+                        command.ExecuteNonQuery();
+                    }
+                    check = true;
+                }
+                catch(Exception ex)
+                {
+                    check = false;
+                }
+
+            }
+            return check;
+        }
+
+        [Route("updateProduct")]
+        [HttpPost]
+        public bool updateProduct(Products products)
+        {
+            bool check = false;
+
+            using (SqlConnection connection = new SqlConnection(PhoController.connectionString))
+            {
+                string sqlQuery = "UPDATE Product SET CateID = @CateID, ProductName = @ProductName, ProductPrice= @ProductPrice, ProductUnit = @ProductUnit  WHERE ProductID = @ProductID";
+                using (SqlCommand command = new SqlCommand(sqlQuery, connection))
+                {
+                    try
+                    {
+                        connection.Open();
+                        command.Parameters.AddWithValue("@CateID", products.CateID);
+                        command.Parameters.AddWithValue("@ProductName", products.ProductName);
+                        command.Parameters.AddWithValue("@ProductPrice", products.ProductPrice);
+                        command.Parameters.AddWithValue("@ProductUnit", products.ProductUnit);
+                        command.Parameters.AddWithValue("@ProductID", products.ProductID);
+                        command.ExecuteNonQuery();
+                        check = true;
+                    }
+                    catch
+                    {
+                        check = false;
+                    }
+                }
+
+            }
+            return check;
+        }
+
+        [Route("deleteProduct")]
+        [HttpPost]
+        public bool deleteProduct(Products products)
+        {
+            bool check = false;
+            if (checkProductIDUnique(products.ProductID) == true)
+            {
+                using (SqlConnection connection = new SqlConnection(PhoController.connectionString))
+                {
+                    string sqlQuery = "Delete from Product where ProductID= '" + products.ProductID + "'";
+                    try
+                    {
+                        using (SqlCommand command = new SqlCommand(sqlQuery, connection))
+                        {
+                            connection.Open();
+                            command.ExecuteNonQuery();
+                        }
+                        check = true;
+                    }
+                    catch
+                    {
+                        check = false;
+                    }
+                }
+            }
+            return check;
+        }
+
     }
 }

118edac9de4947b2b394ae12f566c5a613f0211f
anhduymm1 <84274470+anhduymm1@users.noreply.github.com>
2023-08-09 22:30:30 +0700

    blabla

--- PWA/Content/js/home.js
+++ PWA/Content/js/home.js
@@ -1,6 +1,7 @@
 ﻿$("document").ready(function () {
     getListDeskByWhere(-1);
     statiticDesk();
+
 });
 
 //function getListDesk() {
@@ -167,8 +168,20 @@ function getListDeskByWhere(status) {
 
                 card.on('click', function () {
                     var userFormModal = new bootstrap.Modal(document.getElementById("userformmodal"));
+                    var ModalOrder = new bootstrap.Modal(document.getElementById("modalorder"));
+                    if (item.TableStatus == '1') {
+                        userFormModal.show();
+                    }
+                    else {
+                        ModalOrder.show();
+                        getListProduct('-1')
+                        $("#searchInput").on("input", function () {
+                            var searchText = $(this).val().toLowerCase();
+                            filterProducts(searchText);
+                        });
+                    }
                     detailTable(item.TableName)
-                    userFormModal.show();
+
                 });
 
                 imageDiv.append(image);
@@ -189,8 +202,11 @@ function getListDeskByWhere(status) {
 
 function detailTable(tenban) {
     var tableName = document.getElementById("tenban")
+    var tableName1 = document.getElementById("tenban1")
     tableName.innerText = tenban
+    tableName1.innerText = tenban
     tableName.style.color = 'blue'
+    tableName1.style.color = 'blue'
     document.getElementById("save").style.display = 'none'
     document.getElementById("update").style.display = 'block'
 }
@@ -206,6 +222,86 @@ $('input[name="table-status"]').on('change', function () {
 document.getElementById("userformmodal").addEventListener("show.bs.modal", function () {
     // Clear the form fields when the modal is shown
 });
+
+function getListProduct(id) {
+    var data = {
+        CateID: id
+    }
+    $.ajax({
+        url: '/getListProduct',
+        type: 'POST',
+        dataType: 'json',
+        data, data,
+        success: function (data) {
+            var containerDiv = $('.listproduct');
+            data.forEach(function (item, index) {
+                var cardDiv = $('<div>', {
+                    class: 'col-6 col-lg-3'
+                });
+
+                var card = $('<div>', {
+                    class: 'mb-3',
+                    style: 'height: 100%'
+                });
+
+                // Tạo một thẻ input checkbox
+                var checkboxInput = $('<input>', {
+                    type: 'radio',
+                    class: 'btn-check',
+                    name: "btnradio",
+                    id: 'btnradio' + item.ProductID,
+                    autocomplete: 'off'
+                });
+
+                // Tạo một thẻ label kết hợp với input checkbox
+                var checkboxLabel = $('<label>', {
+                    class: 'btn btn-outline-primary productname',
+                    for: 'btnradio' + item.ProductID,
+                    style: 'width:100%; height:80%;display: flex;align-items: center;justify-content: center;',
+                    text: item.ProductName
+                });
+
+                // Gắn thẻ input checkbox và label vào thẻ card
+                card.append(checkboxInput);
+                card.append(checkboxLabel);
+
+                // Gắn thẻ card vào thẻ cardDiv
+                cardDiv.append(card);
+                containerDiv.append(cardDiv);
+            });
+        },
+        error: function () {
+            console.log('Không thể lấy dữ liệu từ API.');
+        }
+    });
+}
+
+function removeVietnameseDiacritics(str) {
+    return str.normalize('NFD').replace(/[\u0300-\u036f]/g, '');
+}
+
+function filterProducts(searchText) {
+    var normalizedSearchText = removeVietnameseDiacritics(searchText.toLowerCase());
+    var productLabels = $(".productname");
+
+    productLabels.each(function () {
+        var productName = $(this).text();
+        var normalizedProductName = removeVietnameseDiacritics(productName.toLowerCase());
+
+        if (normalizedProductName.indexOf(normalizedSearchText) !== -1) {
+            $(this).closest(".col-6.col-lg-3").show();
+        } else {
+            $(this).closest(".col-6.col-lg-3").hide();
+        }
+    });
+}
+
+
+
+
+
+
+
 
 
 
